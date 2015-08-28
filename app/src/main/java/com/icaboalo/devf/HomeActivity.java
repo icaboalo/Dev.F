738f69d9da7d25888e0e55a1457a41304134184f
unknown <icaboalo@gmail.com>
2015-08-28 18:58:28 -0500

    Initial commit

---
+++ app/src/main/java/com/icaboalo/devf/HomeActivity.java
@@ -0,0 +1,75 @@
+package com.icaboalo.devf;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class HomeActivity extends ActionBarActivity {
+
+    EditText mUsernameInput, mPasswordInput;
+    TextView mPasswordForgot;
+    Button   mLogInButton, mRegisterButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mUsernameInput  = (EditText) findViewById(R.id.username_input);
+        mPasswordInput  = (EditText) findViewById(R.id.password_input);
+        mPasswordForgot = (TextView) findViewById(R.id.password_forgot);
+        mLogInButton    = (Button) findViewById(R.id.login_button);
+        mRegisterButton = (Button) findViewById(R.id.register_button);
+
+        mLogInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mUsernameInput.getText().toString().trim().length() < 4) {
+                    mUsernameInput.setError(getResources().getString(R.string.username_input_error));
+                } else Toast.makeText(HomeActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+                if (mPasswordInput.getText().toString().trim().length() < 6) {
+                    mPasswordInput.setError(getResources().getString(R.string.password_input_error));
+                } else Toast.makeText(HomeActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToRegister = new Intent(HomeActivity.this, RegisterActivity.class);
+                startActivity(goToRegister);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/icaboalo/devf/RegisterActivity.java
@@ -0,0 +1,132 @@
+package com.icaboalo.devf;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class RegisterActivity extends ActionBarActivity {
+    Toolbar  mToolbar;
+    EditText mFirstNameInput, mLastNameInput, mPasswordInput, mPasswordConfirmationInput;
+    TextView mPasswordStrength, mPasswordRepetition;
+    Button mRegisterButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mToolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mFirstNameInput = (EditText) findViewById(R.id.first_name_input);
+        mLastNameInput  = (EditText) findViewById(R.id.last_name_input);
+        mPasswordInput  = (EditText) findViewById(R.id.password_input);
+        mPasswordConfirmationInput = (EditText) findViewById(R.id.password_input_confirmation);
+
+        mPasswordStrength   = (TextView) findViewById(R.id.password_strength);
+        mPasswordRepetition = (TextView) findViewById(R.id.password_repetition);
+
+        mRegisterButton = (Button) findViewById(R.id.register_button);
+
+        mPasswordInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                int password = mPasswordInput.getText().length();
+                if (password <= 4){
+                    mPasswordStrength.setText(getResources().getString(R.string.password_strength_weak));
+                    mPasswordStrength.setTextColor(getResources().getColor(R.color.red));
+                }
+                if (password > 4){
+                    mPasswordStrength.setText(getResources().getString(R.string.password_strength_intermediate));
+                    mPasswordStrength.setTextColor(getResources().getColor(R.color.orange));
+                }
+                if (password >= 8){
+                    mPasswordStrength.setText(getResources().getString(R.string.password_strength_strong));
+                    mPasswordStrength.setTextColor(getResources().getColor(R.color.green));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        mPasswordConfirmationInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String password = mPasswordInput.getText().toString();
+                String passwordRepetition = mPasswordConfirmationInput.getText().toString();
+                if (password.equals(passwordRepetition)){
+                    mPasswordRepetition.setText(getResources().getString(R.string.password_strength_weak));
+                    mPasswordRepetition.setTextColor(getResources().getColor(R.color.green));
+                }else {
+                    mPasswordRepetition.setText(getResources().getString(R.string.password_strength_intermediate));
+                    mPasswordRepetition.setTextColor(getResources().getColor(R.color.red));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mFirstNameInput.getText().toString().trim().length() > 4
+                        && mLastNameInput.getText().toString().trim().length() > 4
+                        && mPasswordInput.getText().toString().trim().length() > 4){
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_register, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == android.R.id.home){
+            Intent gotoLogin = new Intent(RegisterActivity.this, HomeActivity.class);
+            startActivity(gotoLogin);
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
